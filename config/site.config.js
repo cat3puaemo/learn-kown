f45870bd9a670e09e91c96edc2f2b09be2991f1e
CAT21CATA <104726060+CAT21CATA@users.noreply.github.com>
2022-05-01 23:39:16 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,14 +7,14 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'emotina@puaemo.onmicrosoft.com',
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'cat21@puaemo.onmicrosoft.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "水灵君情感课程",
+  title: "好好学习，天天向上",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/03🏃🏻‍♂️撩妹课程会员', '/04💃撩汉课程会员', '/05🈲杏福课会员'],
+  protectedRoutes: ['/❌文件'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:spencer.wushangbo@gmail.com',
@@ -48,14 +48,6 @@ module.exports = {
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
-    {
-      name: '微信',
-      link: 'https://mp.weixin.qq.com/s/5QmOV5ARRsL-7nrzYCGQIA',
-    },
-    {
-      name: 'Telegram',
-      link: 'https://t.me/puabamei',
-    },
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
